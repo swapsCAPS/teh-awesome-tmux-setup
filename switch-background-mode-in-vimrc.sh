3fe6aeb5ff5c12d1c6062bc4a9eaf1953faeac37
swapsCAPS <verstegen.daan@gmail.com>
2022-01-31 17:35:09 +0100

    removed bunch of legacy config

--- switch-background-mode-in-vimrc.sh
+++
@@ -1,3 +0,0 @@
-mode=$1
-sed -i "/^set background=/c\set background=${mode}" ~/src/teh-awesome-vim-setup/.vimrc
-
--- switch-colorscheme-in-vimrc.sh
+++
@@ -1,2 +0,0 @@
-colorschemeName=$1
-sed -i "/^colorscheme /c\colorscheme ${colorschemeName}" ~/src/teh-awesome-vim-setup/.vimrc
